e872285732bf62a2c4fc16bf5bd518b8e2237188
Hector <hector.evalverde@gmail.com>
2022-05-18 18:25:33 +0200

    feat: conversation saving

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,41 @@
 import type {NextPage} from 'next'
 import {supabase} from "../utils/supabase-client";
 import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import {useEffect, useMemo, useState} from "react";
+import { Disclosure} from '@headlessui/react'
 import {classNames} from "../utils/classnames";
 import Image from "next/image";
 import moment from "moment";
+import { makeId } from '../utils/utils'
 
 interface Profile {
     id: string;
     username: string;
     role: number;
 }
+interface Conversation{
+    id: string | undefined;
+    user_id: string;
+    app_selfie: string;
+    user_selfie?: string;
+    created_at: Date;
+    position: number;
+}
 
 const Home: NextPage = () => {
     const router = useRouter()
     const user = supabase.auth.user();
     const [profile, setProfile] = useState<Profile>()
-    const [imageUrls, setImageUrls] = useState<string[]>([])
-    const [userImages, setUserImages] = useState<string[]>([])
-    const [imageCounter, setImageCounter] = useState<number>(0)
+    const [imageUrl, setImageUrl] = useState<string>("")
+    const [userImage, setUserImage] = useState<string>("")
     const [imageUploaded, setImageUploaded] = useState<File>()
     const [endGame, setEndGame] = useState<boolean>(false)
-    const [conversation, setConversation] = useState<any>([])
+    const [conversation, setConversation] = useState<Conversation[] | any>([])
+    const [conversationId, setConversationId] = useState<string>("")
+
+    useMemo(() => {
+        setConversationId(makeId(12))
+    }, [user])
 
     const amountImages: number[] = [1, 2, 3]
     const randomImage: number = amountImages[Math.floor(Math.random() * amountImages.length)]
@@ -35,20 +46,31 @@ const Home: NextPage = () => {
         }
     }, [user])
 
+    useEffect(() => {
+        getProfile()
+    }, [user])
+
+    useEffect(() => {
+        getImageRandom()
+    },[user])
+
+    useEffect(() => {
+        if(conversation.length === 2){
+            saveConversation()
+            setEndGame(true)
+        }
+    }, [conversation])
+
     const getImageRandom = async () => {
         const { publicURL, error } = supabase
             .storage
             .from('bucket')
             .getPublicUrl(`app_selfies/${randomImage}.jpg`  )
         if(publicURL !== null){
-            setImageUrls([...imageUrls, publicURL]);
+            setImageUrl(publicURL);
         }
+    }
 
-    }
-    const handleImage = () => {
-        getImageRandom()
-        setImageCounter(imageCounter + 1)
-    }
     const getProfile = async () => {
         try {
             const {data, error} = await supabase
@@ -60,9 +82,7 @@ const Home: NextPage = () => {
             console.log(err)
         }
     }
-    useEffect(() => {
-        getProfile()
-    }, [setProfile])
+
 
     const handleLogout = async () => {
         const {error} = await supabase.auth.signOut()
@@ -79,13 +99,10 @@ const Home: NextPage = () => {
         setImageUploaded(e.target.files[0])
     }
 
-
     const uploadImage = async (e: any) => {
         try {
-            // setUploading(true)
-
             if(imageUploaded){
-                const fileName = `${profile?.username}_${imageCounter}`
+                const fileName = `${profile?.username}_${moment().format()}`
 
                 let { data, error: uploadError } = await supabase.storage
                     .from('bucket')
@@ -94,37 +111,41 @@ const Home: NextPage = () => {
                     throw uploadError
                 }
                 if(data){
-                    // const {data, error} = await supabase.storage.from('public/bucket').download(fileName)
                     const { publicURL, error } = supabase
                         .storage
                         .from('bucket')
                         .getPublicUrl(fileName)
-                    console.log(publicURL)
-                    // @ts-ignore
-                    // const url = URL.createObjectURL(data)
-                    setUserImages([...userImages, publicURL]);
+                    if(publicURL) {
+                        setUserImage(publicURL)
+                        const firstConversation = {
+                            id: conversationId,
+                            user_id: profile?.id,
+                            app_selfie: imageUrl,
+                            user_selfie: publicURL,
+                            created_at: moment().format()
+                        }
+                        setConversation((array:any) => {
+                            return [...conversation, firstConversation]
+                        })
+                    }
                 }
             }
         } catch (error:any) {
             alert(error.message)
         }
     }
-    if(imageCounter === 4){
-        setEndGame(true)
 
-    }
     const saveConversation = async () => {
-        const conversation = imageUrls.map(image => {
-            return {app_file: image, created_at: Date.now()}
-        })
         const { data, error } = await supabase
             .from('conversations')
-            .upsert([{"id": "123", "user_id": profile?.id,
-                "app_selfie": "blob:http://localhost:3000/b263b476-b434-42b2-a6c7-d70ff969b89e",
-                "user_selfie": "blob:http://localhost:3000/b263b476-b434-42b2-a6c7-d70ff969b89e",
-                "created_at": moment().format()}])
+            .upsert(conversation)
+        if(data) console.log(data);
+        if(error) console.log(error);
     }
-    // saveConversation()
+    const handleNewImage = () => {
+        getImageRandom()
+    }
+
     return (
         <>
             {user ? (
@@ -172,51 +193,24 @@ const Home: NextPage = () => {
                                 </>
                             )}
                         </Disclosure>
-                        <header className="bg-white shadow">
-                            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                                <h1 className="text-3xl font-bold leading-tight text-gray-900">Game</h1>
-                            </div>
-                        </header>
-                        <main>
-                            <div className="relative max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-                                {/* Replace with your content */}
-                                <div className="px-4 py-4 flex  sm:px-0">
-                                    <button
-                                        onClick={handleImage}
-                                        type="button"
-                                        className="absolute top-0 left-0 bg-indigo-600 p-1 rounded-md text-indigo-200 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white"
-                                    >
-                                       New Image
-                                    </button>
-                                </div>
-                                {/*RANDOM IMAGES FROM ALTER EGO*/}
-                                <div className={"flex flex-col absolute top-10 right-4"} >
-                                    {imageUrls[0] && (
+                        <main className={"flex items-center justify-center"}>
+                            <div className="relative w-1/3 h-[40rem] mx-auto my-20  rounded-md py-6 border-2 sm:px-6 lg:px-8">
+                                {endGame && <span>END GAME</span>}
+                                <button className={"w-10 h-10 rounded-full bg-blue-700 z-50 absolute top-2 left-2"} onClick={handleNewImage}></button>
+                                    {imageUrl && (
                                         <div className={"flex flex-col mt-4"}>
-                                            <Image src={imageUrls[0]} width={200} height={200} />
+                                            <Image src={imageUrl} layout={"fill"} className={"rounded-md"}/>
                                             <span>Answer me with the same selfie</span>
                                         </div>
                                     )}
-                                    {imageUrls[1] && (
-                                        <div className={"flex flex-col mt-44"}>
-                                            <Image src={imageUrls[1]} width={200} height={200} />
-                                            <span>Answer me with the same selfie</span>
-                                        </div>
-                                    )}
-                                </div>
                                 {/*IMAGES FROM THE USER*/}
                                 <div className={"flex flex-col absolute top-44 left-4"} >
                                     {/*@ts-ignore*/}
                                     <input type="file" id="mypic" accept="image/*" capture="camera" onChange={handleUploadImage} />
                                     <button onClick={uploadImage} className={"border-2 border-red-500"}>Upload</button>
-                                    {userImages[0] && (
+                                    {userImage && (
                                         <div className={"flex flex-col mt-4"}>
-                                            <Image src={userImages[0]} width={200} height={200} />
-                                        </div>
-                                    )}
-                                    {userImages[1] && (
-                                        <div className={"flex flex-col mt-44"}>
-                                            <Image src={userImages[1]} width={200} height={200} />
+                                            <Image src={userImage} width={200} height={200} />
                                         </div>
                                     )}
                                 </div>
