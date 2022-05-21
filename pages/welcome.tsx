ec536f32497e4d5dd22397d0f6b131e10f0454de
Hector <hector.evalverde@gmail.com>
2022-05-21 20:32:20 +0200

    feat: change QR

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -9,7 +9,7 @@ const Welcome: NextPage = () => {
             <div
                 className="w-full flex flex-col items-center justify-center h-full border-[1px] border-whiteBorder rounded-[25px] m-2">
                 <div className="py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                    <div className={"w-64 h-80  relative flex items-center justify-center"}>
+                    <div className={"w-56 h-72  relative flex items-center justify-center"}>
                         <div className={"w-4 h-4 border-t-2 border-l-2 border-white absolute top-0 left-0"}>
                         </div>
                         <div className={"w-4 h-4 border-t-2 border-r-2 border-white absolute top-0 right-0"}>
@@ -18,10 +18,10 @@ const Welcome: NextPage = () => {
                         </div>
                         <div className={"w-4 h-4 border-b-2 border-r-2 border-white absolute bottom-0 right-0"}>
                         </div>
-                        <div className={"bg-dark flex flex-col items-center absolute top-12 w-44 h-44"}>
+                        <div className={"bg-dark flex flex-col items-center absolute top-4 w-56 h-56"}>
                             <Image src={"/qr.png"} layout={"fill"} className={"border-2 "}/>
                         </div>
-                        <span className={"text-white text-xl absolute bottom-12"}>SCAN ME</span>
+                        <span className={"text-white text-xl absolute bottom-4"}>SCAN ME</span>
                     </div>
 
                 </div>
